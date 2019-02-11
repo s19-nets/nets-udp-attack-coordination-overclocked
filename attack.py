e6314e7741a514de2195f79407354c613114ad53
SpottieTea <noashea.d@gmail.com>
2019-02-11 13:04:03 -0700

    Parsing input.

--- attack.py
+++ attack.py
@@ -1,8 +1,22 @@
 #! /usr/bin/env python3
 
-
-import time, os
+import time, os, sys, re
 pid = os.getpid()
+
+
+#get params
+#Assumning input is correct, we have the following variables
+addr1,port1 = sys.argv[2].split(":") #listening port
+addr2,port2 = sys.argv[4].split(":") #sending port
+
+port1 = int(port1)
+port2 = int(port2)
+
+
+print("\n")
+print(pid,"Recieved listening port",port1)
+print(pid,"Recieved sending port", port2)
+print("\n")
 
 print("dummy attack program with pid %d.  do not trust my results" % pid)
 
