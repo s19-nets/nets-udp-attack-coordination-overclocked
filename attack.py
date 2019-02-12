2c02a3eb9a8254624c81ee785f6ae94df5b1418b
Taylor Dodson <t.raynedod@gmail.com>
2019-02-12 08:29:48 -0700

    Try block. Renamed vars. Init sockets.

--- attack.py
+++ attack.py
@@ -1,21 +1,35 @@
 #! /usr/bin/env python3
 
 import time, os, sys, re
+from socket import *
+
 pid = os.getpid()
+
+#defaults
+lAddr = ("", 50000) # listening addr+port
+sAddr = ("", 50010) # sending addr+port
+
+def usage():
+    print ("usage: -l <addr:port> -s <addr:port>]")
 
 
 #get parameters
 #Assumning input is correct, we have the following variables
-addr1,port1 = sys.argv[2].split(":") #listening port
-addr2,port2 = sys.argv[4].split(":") #sending port
+try:
+    lAddr,port1 = sys.argv[2].split(":")
+    sAddr,port2 = sys.argv[4].split(":")
+except:
+    usage()
 
 port1 = int(port1)
 port2 = int(port2)
 
+lSocket = socket(AF_INET, SOCK_DGRAM) # listening socket
+sSocket = socket(AF_INET, SOCK_DGRAM) # sending socket
 
 print("\n")
-print(pid,"Recieved listening port",port1)
-print(pid,"Recieved sending port", port2)
+print(pid, lAddr, ": listening on port",port1)
+print(pid, sAddr, ": sending on port", port2)
 print("\n")
 
 print("dummy attack program with pid %d.  do not trust my results" % pid)
